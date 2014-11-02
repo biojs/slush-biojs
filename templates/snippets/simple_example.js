07aa745151a826bfb3ce9987c4b7abc48e9ac43e
Sebastian Wilzbach <sebi@wilzbach.me>
2014-11-02 04:26:29 +0100

    create a new instance in the default snippet

--- templates/snippets/simple_example.js
+++ templates/snippets/simple_example.js
@@ -1,3 +1,3 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("<%= appNameSlug %>");
-var instance = app({el: yourDiv, text: 'biojs'});
+var instance = new app({el: yourDiv, text: 'biojs'});
